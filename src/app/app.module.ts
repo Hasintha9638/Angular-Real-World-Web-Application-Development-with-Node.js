514c662a43f3891ee178968e74deccbb9cfbaafe
Hasintha9638 <hasinthaeanaweera08@gmail.com>
2021-01-17 21:30:23 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { AboutComponent } from './about/about.component';
+import { MyMaterialModule } from './material';
+import { ArticleService } from './article.service';
+import { ArticleComponent } from './article/article.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    ArticleListComponent,
+    AboutComponent,
+      ArticleComponent,
+      NotFoundComponent
+   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    MyMaterialModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/article.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Article } from './article';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+constructor(private http:HttpClient) { }
+
+  getArticles():Observable<Article[]>{
+    
+    return this.http.get<Article[]>("http://localhost:8080/articles");
+  }
+
+  getArticle(key:string):Observable<Article>{
+    
+    return this.http.get<Article>("http://localhost:8080/articles/"+key);
+  }
+
+}
